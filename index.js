34887fc971f75ac4a444bd1da458da1e5704050d
Muhammad Shahab Rasheed <shahab897@gmail.com>
2023-01-24 23:30:12 +0500

    simple code to add text to docx and save file

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const pdfTextExtract = require('pdf-text-extract');
+const officegen = require('officegen');
+const fs = require('fs');
 const options = { layout: 'raw' };
+
 pdfTextExtract.extract('path/to/pdf', options, function (err, pages) {
     if (err) {
         console.dir(err)
@@ -9,3 +12,18 @@ pdfTextExtract.extract('path/to/pdf', options, function (err, pages) {
         console.log(page)
     });
 });
+
+
+/*simple code to add text to docx and save*/
+// Create a new Word document
+const docx = officegen('docx');
+
+// Set the title of the document
+docx.title('Converted PDF');
+
+// Add text to the document
+docx.addText('Text extracted from PDF file', { font_face: 'Arial', font_size: 14 });
+
+// Save the document
+const stream = fs.createWriteStream('./path/to/output.docx');
+docx.generate(stream);
