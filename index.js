3cc7fa29b22a0e50c2b3f35830b79afb16af3f5a
Muhammad Shahab Rasheed <shahab897@gmail.com>
2023-10-03 00:32:09 +0500

    Update: Include url Shortner in index file

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const path = require("path");
 const { faker } = require("@faker-js/faker");
 const PORT = process.env.PORT || 3000;
 const app = express();
+
 const pdfToDocx = require("./Tools/pdfToDocx");
 const passwordGenerator = require("./Tools/passwordGenerator");
 const rss = require("./Tools/rss");
 const ImageResizer = require("./Tools/imageResizer");
+const urlShortner = require("./Tools/urlShortner");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -28,40 +30,7 @@ app.use("/api/convert", pdfToDocx);
 app.use("/api/password", passwordGenerator);
 app.use("/api/rss", rss);
 app.use("/api/resize", ImageResizer);
-
-function generateShortUrl() {
-  const chars =
-    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-  let shortUrl = "";
-
-  for (let i = 0; i < 6; i++) {
-    const randomIndex = Math.floor(Math.random() * chars.length);
-    shortUrl += chars[randomIndex];
-  }
-
-  return shortUrl;
-}
-
-app.post("/api/shortenurl", function (req, res) {
-  const longUrl = req.body.longUrl;
-  // use this code to get baseurl when in production
-  // const baseUrl = req.protocol + '://' + req.hostname + req.baseUrl;
-
-  if (!longUrl) {
-    res.status(400).send("Missing longUrl parameter");
-    return;
-  }
-
-  let shortUrl;
-  do {
-    shortUrl = generateShortUrl();
-  } while (db[shortUrl]);
-
-  db[shortUrl] = longUrl;
-  fs.writeFileSync(dbFilePath, JSON.stringify(db));
-
-  res.json({ shortUrl: `http://localhost:${PORT}/${shortUrl}` });
-});
+app.use("/api/shortenurl", urlShortner);
 
 app.get("/:shortUrl", function (req, res) {
   const longUrl = db[req.params.shortUrl];
